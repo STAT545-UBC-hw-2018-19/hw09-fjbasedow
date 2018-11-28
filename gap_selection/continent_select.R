0f691573e76b4f8932052feece5856e0631d29d3
Frederike Basedow <f.basedow@arcor.de>
2018-11-27 20:09:53 -0800

    add comments

--- gap_selection/continent_select.R
+++ gap_selection/continent_select.R
@@ -1,4 +1,7 @@
 library(tidyverse)
+
+# make function that filters gapminder data by continent of choice, 
+# groups it by country and calculates mean life expectancy by country  
 continent_select <- function(cont) {
 	library(tidyverse)
 	library(gapminder)
@@ -6,5 +9,5 @@ continent_select <- function(cont) {
 		filter(continent == cont) %>% 
 		group_by(country) %>%
 		summarize(mean_lifeExp = mean(lifeExp))  %>% 
-		write_csv(paste0(cont, "/", cont,"_selection.csv"))
+		write_csv(paste0(cont, "/", cont,"_selection.csv")) # save as csv with name and directory depending on continent selection
 }
--- gap_selection/gap_plot.R
+++ gap_selection/gap_plot.R
@@ -1,17 +1,17 @@
-library(tidyverse)
+# make function to plot lifeExp data for each country for specific continent
 gap_plot <- function(data, cont) {
 	data %>% 	mutate(country = fct_reorder(country, mean_lifeExp)) %>% 
-ggplot(aes(country, mean_lifeExp)) +
-	geom_point(colour = "hotpink") +
-	labs(
-		title = paste("Mean Life Expectancy in", cont),
-		x = "",
-		y = "Mean Life Expectancy (years)"
-	) +
-	theme_bw() +
-	theme(axis.text = element_text(
-		angle = 90,
-		hjust = 1
-	))
-	ggsave(paste0(cont,"_plot.png"), path = paste0("./", cont))
+		ggplot(aes(country, mean_lifeExp)) + 
+		geom_point(colour = "hotpink") +
+		labs(
+			title = paste("Mean Life Expectancy in", cont),
+			x = "",
+			y = "Mean Life Expectancy (years)"
+		) +
+		theme_bw() +
+		theme(axis.text = element_text(
+			angle = 90,
+			hjust = 1
+		))
+	ggsave(paste0(cont,"_plot.png"), path = paste0("./", cont)) # save as .png with name and directory depending on continent selection
 }
